fbdb6bb9b1590b0273a6debaf834685a93e16815
IamSuperB4 <seahawks2000@live.com>
2021-10-25 10:33:19 -0700

    Stub Functions

---
+++ DatabaseAccess.js
@@ -0,0 +1,28 @@
+function getBusinessFromDatabase(businessName) {
+	// TODO:
+	// This function takes a name of a business as a parameter
+	// It will go to the database and get all of the information about that business
+	
+	// Returns the data extracted from the database as a JSON object
+ 
+	Return JSON.stringify({});
+}
+
+function getSelectedCategoriesFromDatabase(categories) {
+	// TODO:
+	// This function takes an array of categories as a parameter
+	// It will go to the database and get a random activity from that category
+	
+	// Returns the data extracted from the database as a JSON object
+ 
+	Return JSON.stringify([]);
+}
+ 
+function getAllActivitiesFromDatabase() {
+	// TODO:
+	// This function goes to the database and gets every activity in the database
+	
+	// Returns the data extracted from the database as a JSON object
+
+	Return JSON.stringify([]);
+}
---
+++ script.js
@@ -0,0 +1,44 @@
+ function goToChooseActivites() {
+    // TODO: This function is triggered when the user clicks the Create Adventure button on the landing page
+	//  Loads the HTML of the "Choose number of activities" step
+}
+ 
+function whenActivityCountSelected() {
+    // TODO: This function is  triggered when the user has selected a number of activities
+	// 	The next button will turn green and be clickable, and the CSS will change to 
+	//  show the category is highlighted.
+}
+ 
+function goToMoodSelect() {
+    // TODO: This function is triggered when the user clicks the Next button on the Choose Number of Activies Page
+	//  Loads the HTML of the "Mood Select" step.
+	//  Prompts the user to select as many activities as they selected on the Choose Number of Activies Page
+}
+ 
+function whenMoodSelected() {
+    // TODO: This function is triggered when the user has selected at one mood is selected
+	// 	The next button will turn green and be clickable, and the CSS will change to 
+	//  show the category is highlighted.
+}
+ 
+function goToFinalAdventure(numStops, numCategory) {
+    // TODO: This function generates HTML for the adventure for the user
+	// Based on the number of stops and categories the user
+	//    has selected.
+}
+ 
+function refreshRandomActivity(category) {
+	// TODO:
+	// This function is triggered when a user presses the refresh button on one of the activities on their adventure
+	// It will go to the database to get a new activity from the same category
+	// Once the new activity is extracted from the database, a new DOM object will be created to replace the old activity with the new activity data
+}
+ 
+function directoryCategorySelected() {
+	// TODO:
+	// This function is triggered when a user clicks a category in the directory page
+	// If the category is collapsed, it will expand the category to show the activities available for that category
+	// If the category is not collapsed, it will collapse it
+}
+ 
+
